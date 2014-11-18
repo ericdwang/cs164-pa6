30d60477de87cc633a3a25663ccf69c01fa13ed5
Eric Wang <ericdwang@tbp.berkeley.edu>
2014-11-17 22:48:34 -0800

    Fix models and database such that Django queries now work

--- congress/senate/models.py
+++ congress/senate/models.py
@@ -1,35 +1,35 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
 
-# Create your models here.
 class State(models.Model):
-    statecode = models.CharField(max_length=2, primary_key=true)
+    statecode = models.TextField(primary_key=True)
     population_2010 = models.IntegerField()
     population_2000 = models.IntegerField()
     population_1950 = models.IntegerField()
     population_1900 = models.IntegerField()
     landarea = models.FloatField()
-    name = models.CharField(max_length=50)
-    admitted_to_union = models.CharField(max_length=50)
+    name = models.TextField()
+    admitted_to_union = models.TextField()
+
 
 class County(models.Model):
-    name = models.CharField(max_length=50)
-    statecode = models.ForeignKey(State, to_field='statecode')
+    name = models.TextField()
+    statecode = models.ForeignKey(State)
     population_1950 = models.IntegerField()
     population_2010 = models.IntegerField()
 
+
 class Senator(models.Model):
-    statecode = models.ForeignKey(State, to_field='statecode')
-    name = models.CharField(max_length=100, primary_key=true)
-    affiliation = models.CharField(max_length=100)
+    name = models.TextField(primary_key=True)
+    statecode = models.ForeignKey(State)
+    affiliation = models.TextField()
     took_office = models.IntegerField()
     born = models.IntegerField()
 
+
 class Committee(models.Model):
-    id = models.CharField(max_length=100, primary_key=true)
-    parent_committee = models.ForeignKey(Committee, to_field='id')
-    name = models.CharField(max_length=100)
-    chairman = models.ForeignKey(Senator, to_field='name')
-    ranking_member = models.ForeignKey(Senator, to_field='name')
+    id = models.TextField(primary_key=True)
+    parent_committee = models.ForeignKey('self')
+    name = models.TextField()
+    chairman = models.ForeignKey(Senator)
+    ranking_member = models.ForeignKey(Senator)
