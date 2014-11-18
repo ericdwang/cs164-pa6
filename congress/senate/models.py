ffac826d7fdd52cf0700aa06a37df9bf6bb5efb7
Eric Wang <ericdwang@tbp.berkeley.edu>
2014-11-17 23:24:16 -0800

    Modify database and models to use original naming convention

--- congress/senate/models.py
+++ congress/senate/models.py
@@ -11,25 +11,37 @@ class State(models.Model):
     name = models.TextField()
     admitted_to_union = models.TextField()
 
+    class Meta(object):
+        db_table = 'states'
+
 
 class County(models.Model):
     name = models.TextField()
-    statecode = models.ForeignKey(State)
+    statecode = models.ForeignKey(State, db_column='statecode')
     population_1950 = models.IntegerField()
     population_2010 = models.IntegerField()
+
+    class Meta(object):
+        db_table = 'counties'
 
 
 class Senator(models.Model):
     name = models.TextField(primary_key=True)
-    statecode = models.ForeignKey(State)
+    statecode = models.ForeignKey(State, db_column='statecode')
     affiliation = models.TextField()
     took_office = models.IntegerField()
     born = models.IntegerField()
 
+    class Meta(object):
+        db_table = 'senators'
+
 
 class Committee(models.Model):
     id = models.TextField(primary_key=True)
-    parent_committee = models.ForeignKey('self')
+    parent_committee = models.ForeignKey('self', db_column='parent_committee')
     name = models.TextField()
-    chairman = models.ForeignKey(Senator)
-    ranking_member = models.ForeignKey(Senator)
+    chairman = models.ForeignKey(Senator, db_column='chairman')
+    ranking_member = models.ForeignKey(Senator, db_column='ranking_member')
+
+    class Meta(object):
+        db_table = 'committees'
