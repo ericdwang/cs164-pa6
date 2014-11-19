c0b8a1af6b882a965bdd335bef0172ddf3249518
Eric Wang <ericdwang@tbp.berkeley.edu>
2014-11-18 17:10:17 -0800

    Add next 2 Python queries

--- congress/queries.py
+++ congress/queries.py
@@ -1,3 +1,6 @@
+from django.db.models import Avg
+from django.db.models import Count
+
 from senate.models import County
 
 
@@ -5,12 +8,32 @@ from senate.models import County
 # ./manage.py shell
 # >>> execfile('queries.py')
 
-print(' ')
 print('Query 1')
 
-counties = County.objects.filter(
-    population_2010__gt=2000000).values(
-        'statecode', 'name', 'population_2010').order_by('-population_2010')
+counties = County.objects.filter(population_2010__gt=2000000) \
+    .values('statecode', 'name', 'population_2010') \
+    .order_by('-population_2010')
+
 for county in counties:
     print('{}|{}|{}'.format(
         county['statecode'], county['name'], county['population_2010']))
+
+print('\nQuery 2')
+
+counties = County.objects.all() \
+    .values('statecode') \
+    .annotate(counties_per_state=Count('statecode')) \
+    .order_by('counties_per_state')
+
+for county in counties:
+    print('{}|{}'.format(county['statecode'], county['counties_per_state']))
+
+print('\nQuery 3')
+
+avg_counties = County.objects.all() \
+    .values('statecode') \
+    .annotate(counties_per_state=Count('statecode')) \
+    .aggregate(avg_counties=Avg('counties_per_state')) \
+    .values()[0]
+
+print(avg_counties)
